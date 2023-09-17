1a75a8cef1abc6c13463ade9aad398d42d16a4bf
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-17 11:44:05 +0300

    1.1 javascript

---
+++ 1/exe1-1/exe.js
@@ -0,0 +1,35 @@
+//String Variable
+let city = "Jerusalem"; 
+
+// Number Variable
+let year = 2023; 
+
+// Object Variable
+let Details = {
+  Population: 874186,
+  Local_Time: "GMT +3",
+  Elevation: "754 m",
+  Area: "125.1 km²",
+  Founded: "3000 BC",
+  Area_code: "+972-2",
+};
+
+// Boolean Variable
+let Capital = true; 
+
+// null Variable
+let time = null; 
+
+// Undefined variable
+let date; 
+
+//Symbol variable
+let currency = Symbol("₪");
+
+console.log(typeof(city));
+console.log(typeof(time));
+console.log(typeof(date));
+console.log(typeof(year));
+console.log(typeof(Details));
+console.log(typeof(Capital));
+console.log(typeof(currency));
