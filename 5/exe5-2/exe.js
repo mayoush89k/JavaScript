f2dec3260a4c1efd0f3728ad07cbd6dcb13ecb67
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-17 18:22:15 +0300

    5-2 languages

--- 5/exe5-2/exe.js
+++ 5/exe5-2/exe.js
@@ -5,20 +5,20 @@ switch (language) {
   case "mandarin":
     console.log("MOST number of native speakers!");
     break;
-  case "Spanish":
+  case "spanish":
     console.log("2nd place in a number of native speakers");
     break;
-  case "English":
+  case "english":
     console.log("3rd place");
     break;
-  case "Hindi":
+  case "hindi":
     console.log("Number 4");
     break;
-  case "Arabic":
+  case "arabic":
     console.log("5th most spoken language");
     break;
 
   default:
-    "Not in the top 5";
+    console.log("Not in the top 5");
     break;
 }
