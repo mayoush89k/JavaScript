94f814d512dc0e1a0321bb320d174e92b43f5c5e
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-17 15:58:35 +0300

    5 - 1 Country to live in

--- 5/exe5-1/exe.js
+++ 5/exe5-1/exe.js
@@ -3,20 +3,22 @@ let isIsland = prompt("In Island? (yes / no)");
 let population = prompt("How large population is in the country?(M)");
 let country = prompt("Name the Country");
 
-language = language.toLowerCase()
+language = language.toLowerCase();
 population = Number(population) * 1000000;
 country = country.toLowerCase();
-if(isIsland == "yes" || isIsland == 'y'){
-    isIsland = true;
+if (isIsland == "yes" || isIsland == "y") {
+  isIsland = true;
 }
-if(isIsland == "no" || isIsland == 'n'){
-    isIsland = false;
+if (isIsland == "no" || isIsland == "n") {
+  isIsland = false;
 }
-if(language.includes('en') && !isIsland && population < 50000000){
-    console.log('You should live in ' + country);
-}else{
-    console.log(language.includes('en'));
-    console.log(!isIsland);
-    console.log(population < 50000000);
-    console.log(country.toUpperCase() + ' doesn\'t meet your criteria');
+// according to sarah's wish no island, less than 50 m and english language
+if (
+  (language.includes("en") && !isIsland && population < 50000000) ||
+  // another condition that lives in island, more than 10 m and speaks Italian
+  (language.includes("it") && isIsland && population > 10000000)
+) {
+  console.log("You should live in " + country);
+} else {
+  console.log(country.toUpperCase() + " doesn't meet your criteria");
 }
