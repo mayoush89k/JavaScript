4b8bd6ab50718f0047f076c07c1a873c9638c024
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-18 15:40:10 +0300

    6 - 4 Loop Arrays

---
+++ 6/exe6-4/exe.js
@@ -0,0 +1,15 @@
+const populations = [20, 60, 46, 24, 36, 48, 79];
+
+function populationPercentages(populations) {
+  const percentage = [];
+  for (let i = 0; i < populations.length; i++) {
+    percentage.push(populations[i]);
+    /*On each iteration use the function we created earlier
+(module 3, ex.3.3-declarations vs expressions) for each
+element of the array*/
+//! because we had not work with module 3 so I just push the number as it is for now
+  }
+  return percentage;
+}
+console.log("result ", populationPercentages(populations));
+
