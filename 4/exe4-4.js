f2299cadb9ebee6d1d3e8c817d5a9a4c878cd10b
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-20 22:16:28 +0300

    4 - 4 BOOM

--- 4/exe4-4.js
+++ 4/exe4-4.js
@@ -1,7 +1,12 @@
-printNum(10);
+printNum(200);
 
 function printNum(number) {
   for (let i = 1; i <= number; i++) {
-    console.log("number: ", i);
+    if (!(i % 7)) {
+      let fullNum = i + "";
+      console.log(fullNum.includes(7) ? "BOOM BOOM" : "BOOM");
+    } else {
+      console.log(i);
+    }
   }
 }
