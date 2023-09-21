e85916e38d9e6ccb516a073d13b7183acaf25881
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-22 00:05:26 +0300

    editing code

--- 6/exe6-4/exe.js
+++ 6/exe6-4/exe.js
@@ -1,14 +1,14 @@
-const populations = [20, 60, 46, 24, 36, 48, 79];
+const populations = [202, 603, 464, 245, 366, 487, 798];
+let world_population = 7900;
 
 function populationPercentages(populations) {
   const percentage = [];
   for (let i = 0; i < populations.length; i++) {
-    percentage.push(populations[i]);
+    percentage.push(((populations[i]/world_population) * 100).toFixed(2));
     /*On each iteration use the function we created earlier
 (module 3, ex.3.3-declarations vs expressions) for each
 element of the array*/
-//! because we had not worked with module 3 so I just push the number as it is for now
-  }
+ }
   return percentage;
 }
 console.log("result ", populationPercentages(populations));
